1aa136d64b791824e832f30c04913d3a99367ed9
Johannes Link <business@johanneslink.net>
2018-12-04 07:47:16 +0100

    Implemented Euro as far as necessary

--- src/main/java/fau/junit5/Euro.java
+++ src/main/java/fau/junit5/Euro.java
@@ -1,15 +1,41 @@
 package fau.junit5;
 
 public class Euro {
-	public Euro(double amount) {
+	private final double amount;
 
+	public Euro(double amount) {
+		if (amount < 0) {
+			throw new IllegalArgumentException("Amount must not be negative");
+		}
+		this.amount = amount;
 	}
 
 	public double getAmount() {
-		return 0;
+		return amount;
 	}
 
-	public Euro plus(Euro two) {
-		return new Euro(0);
+	public Euro plus(Euro addend) {
+		return new Euro(amount + addend.amount);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Euro euro = (Euro) o;
+
+		return Double.compare(euro.amount, amount) == 0;
+	}
+
+	@Override
+	public int hashCode() {
+		long temp = Double.doubleToLongBits(amount);
+		return (int) (temp ^ (temp >>> 32));
+	}
+
+	@Override
+	public String toString() {
+		return String.format("EUR %s", amount);
 	}
 }
