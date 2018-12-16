545b370ecc3a4ac0fdf08a72ee1bbca889d3eb64
Johannes Link <business@johanneslink.net>
2018-12-16 12:07:28 +0100

    Minor renames in tests

--- src/test/java/fau/junit5/JavaAdditionTests.java
+++ src/test/java/fau/junit5/JavaAdditionTests.java
@@ -7,13 +7,14 @@ class JavaAdditionTests {
 	// Delete all test methods before demo
 
 	@Test
-	void weCanAddTwoIntegers() {
+	void intsCanBeAdded() {
 		int sum = 5 + 6;
 		Assertions.assertEquals(11, sum);
+		Assertions.assertEquals(-13, 3 + 7 + (-23));
 	}
 
 	@Test
-	void weCanAddDoubles() {
+	void doublesCanBeAdded() {
 		double aTenth = 0.1;
 		double sum = aTenth + aTenth + aTenth;
 		Assertions.assertEquals(0.3, sum, 0.001);
